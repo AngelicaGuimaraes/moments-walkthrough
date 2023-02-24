31f9f8b879887ae7e6e50e5a102bf5302cc34cca
AngelicaGuimaraes <angelicaguimaraes76@gmail.com>
2023-02-24 13:29:21 +0000

    create CurrentUserContext file

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import Form from "react-bootstrap/Form";
@@ -17,7 +17,7 @@ import appStyles from "../../App.module.css";
 import { SetCurrentUserContext } from "../../App";
 
 function SignInForm() {
-  const setCurrentUser = useContext(SetCurrentUserContext);
+  const setCurrentUser = useSetCurrentUser();
 
   const [signInData, setSignInData] = useState({
     username: "",
